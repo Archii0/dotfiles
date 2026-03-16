4ea1e28c34141ce2814c8e95b9e66693f8b631b0
Archii0 <archiedascombe@icloud.com>
2026-03-16 21:31:59 +0000

    nvim: changes idk

---
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,8 @@
+-- Set leader key
+--vim.g.mapleader = ' '
+--vim.g.maplocalleader = ' '
+
+-- Disable the spacebar key's default behaviour in Normal and Visual modes
+--vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+--local opts = { noremap = true, silent = true }
